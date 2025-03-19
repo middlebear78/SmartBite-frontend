8e076a42af0651917c3925cc44c281fb1ff55b27
middlebear78 <urisham@gmail.com>
2025-03-19 15:38:17 +0200

    more fixes

--- app/Login.tsx
+++ app/Login.tsx
@@ -22,9 +22,9 @@ export default function LoginScreen() {
   const router = useRouter();
   const [userInfo, setUserInfo] = React.useState<UserInfo>(null);
   const [request, response, promptAsync] = Google.useAuthRequest({
-    webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
-    androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
-    iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
+    webClientId: process.env.GOOGLE_WEB_CLIENT_ID,
+    androidClientId: process.env.GOOGLE_ANDROID_CLIENT_ID,
+    iosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
   });
 
   const [isAppleAuthAvailable, setIsAppleAuthAvailable] =
